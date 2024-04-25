1170fa8c79f9a335ec4a8b53b994f7512255bc36
Sarwak <sarwakpro@gmail.com>
2024-04-25 15:30:23 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,9 +54,7 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity) {
     HashMap * map = (HashMap*)malloc(sizeof(HashMap));
 
-  for (int i = 0; i <= capacity; i++) {
-    map->buckets[i] = NULL;
-  }
+    map->buckets = (Pair**)calloc(capacity,sizeof(Pair*));
     map->size = 0;
     map->capacity = capacity;
     map->current = -1;
