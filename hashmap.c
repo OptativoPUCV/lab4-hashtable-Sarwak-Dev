5e5f30fed7eac43bd808e494c926b42137fa6ca3
Sarwak <sarwakpro@gmail.com>
2024-04-25 15:27:04 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,8 +52,15 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
+    HashMap * map = (HashMap*)malloc(sizeof(HashMap));
 
-    return NULL;
+  for (int i = 0; i <= capacity; i++) {
+    map->buckets[i] = NULL;
+  }
+    map->size = 0;
+    map->capacity = capacity;
+    map->current = -1;
+    return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
